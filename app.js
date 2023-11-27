8b12b47d82fb89a2b06b708de51496c39dbd6421
robenUW <112424694+robenUW@users.noreply.github.com>
2023-11-27 12:10:15 -0500

    google fix

--- app.js
+++ app.js
@@ -1,77 +1,12 @@
-// Initialize and add the map
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 }
 
 initMap();
-
-
-
-    ]
-
-    // loop through marker
-    for (let i = 0; i < MarkerArray.length; i++){
-        addMarker(MarkerArray[i]);
-
-    }
-
-    // Add Marker
-
-    function addMarker(property){
-
-        const marker = new google.maps.Marker({
-            position:property.location,
-            map:map,
-            //icon: property.imageIcon
-            });
-
-            // Check for custom Icon
-
-            if(property.imageIcon){
-                // set image icon
-                marker.setIcon(property.imageIcon)
-            }
-
-            if(property.content){
-
-            const detailWindow = new google.maps.InfoWindow({
-            content: property.content
-    });
-    
-    marker.addListener("mouseover", () =>{
-        detailWindow.open(map, marker);
-    })
-}
-
-         
-
-
-
-    }
-
-    
-
-
-
-}
