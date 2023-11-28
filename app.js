fa1059febf25cc2b20cb4342d76b5aa331f03fab
robenUW <112424694+robenUW@users.noreply.github.com>
2023-11-28 15:12:29 -0500

    fix zoom level

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById("map"), {
     center: sydney,
-    zoom: 15,
+    zoom: 5,
   });
 
 // Create the search box and link it to the UI element.
