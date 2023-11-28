d18b14edd78c219865cb815aeb0b95692da0cbd3
robenUW <112424694+robenUW@users.noreply.github.com>
2023-11-28 13:48:21 -0500

    combine places & geolocation api

--- app.js
+++ app.js
@@ -1,42 +1,65 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-let map; 
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+let map;
 let service;
-let temp_lat = '';
-let temp_lng = '';
-let infowindow;
-getLocation();
-
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    temp_lat = 51.507222; //set default lat value
-    temp_lng = -0.1275; //set default lng value
-  }
-}
-
-function showPosition(position) {
-  temp_lat = position.coords.latitude;
-  temp_lng = position.coords.longitude;
-  initMap();
-}
-
+let infoWindow;
 
 function initMap() {
-  const mylocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  const sydney = new google.maps.LatLng(-33.867, 151.195);
 
-  infowindow = new google.maps.InfoWindow();
   map = new google.maps.Map(document.getElementById("map"), {
-    center: mylocation,
+    center: sydney,
     zoom: 15,
   });
 
+  infoWindow = new google.maps.InfoWindow();
+///get location
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        },
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation.",
+  );
+  
+//places
+
+
+
   const request = {
-    query: "Crossfit Ascend",
+    query: "Museum of Contemporary Art Australia",
     fields: ["name", "geometry"],
   };
 
