6e2fed2f111ec03ce3bb9aca93487cbe13233bd4
robenUW <112424694+robenUW@users.noreply.github.com>
2023-11-28 12:18:39 -0500

    location places

--- app.js
+++ app.js
@@ -4,85 +4,63 @@
 // locate you.
 let map; 
 let service;
+let temp_lat = '';
+let temp_lng = '';
 let infoWindow;
-//comment
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
+getLocation();
 
-  infoWindow = new google.maps.InfoWindow();
-  infowindow2 = new google.maps.InfoWindow();
 
-  const request = {
-    query: "Crossfit",
-    fields: ["name", "geometry"],
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-
-      map.setCenter(results[0].geometry.location);
-    }
-  });
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    temp_lat = 51.507222; //set default lat value
+    temp_lng = -0.1275; //set default lng value
+  }
 }
 
-  const locationButton = document.createElement("button");
+function showPosition(position) {
+  temp_lat = position.coords.latitude;
+  temp_lng = position.coords.longitude;
+  initMap();
+}
 
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        },
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
+function initMap() {
+  var location = {
+    lat: temp_lat,
+    lng: temp_lng
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: location,
+    zoom: 15
   });
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+      location: location,
+      radius: 50000,
+      keyword: ['bar']
+    },
+    callback);
+}
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation.",
-  );
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
 }
 
 function createMarker(place) {
-    if (!place.geometry || !place.geometry.location) return;
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
   
-    const marker = new google.maps.Marker({
-      map,
-      position: place.geometry.location,
-    });
-  
-    google.maps.event.addListener(marker, "click", () => {
-      infowindow2.setContent(place.name || "");
-      infowindow2.open(map);
-    });
-}
-  
-window.initMap = initMap;
+  });}
