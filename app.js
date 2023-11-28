a7c336483194e517380651135d24588033ee5c27
robenUW <112424694+robenUW@users.noreply.github.com>
2023-11-28 15:05:44 -0500

    REMOVE bar

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function initMap() {
   infoWindow2 = new google.maps.InfoWindow();
 
   const request = {
-    query: "crossfit ascend",
+    query: "crossfit",
     fields: ["name", "geometry"],
   };
 
@@ -40,71 +40,8 @@ function createMarker(place) {
   const marker = new google.maps.Marker({
     map,
     position: place.geometry.location,
+    label: labels[labelIndex++ % labels.length],
   });
-
-  const input = document.getElementById("pac-input");
-  const searchBox = new google.maps.places.SearchBox(input);
-
-  map.controls[google.maps.ControlPosition.BOTTOM_RIGHT].push(input);
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  let markers = [];
-
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
-
-    places.forEach((place) => {
-      if (!place.geometry || !place.geometry.location) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
-
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        }),
-      );
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
-
 
   ///get location
   const locationButton = document.createElement("button");
@@ -136,6 +73,8 @@ function createMarker(place) {
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
+}
+
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(
